92b0e4bfd779762ae88b3ba0977591d2c9f9bb5d
sinhvien <sinhvien>
2019-03-25 11:15:32 +0700

    edit layout commit

--- BigSchool/BigSchool/Controllers/CoursesController.cs
+++ BigSchool/BigSchool/Controllers/CoursesController.cs
@@ -1,5 +1,6 @@
 ﻿using BigSchool.Models;
 using BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,28 @@ namespace BigSchool.Controllers
         public CoursesController()
         {
             _dbContext = new ApplicationDbContext();
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CoursesViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
         // GET: Courses
         public ActionResult Create()
