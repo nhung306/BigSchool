341054be7f9dd2c2ac106af87810d7cb2b93a0ce
sinhvien <sinhvien>
2019-03-25 10:15:41 +0700

    edit layout commit

--- BigSchool/BigSchool/Migrations/201903250222278_PopulateCategoryTable.cs
+++ BigSchool/BigSchool/Migrations/201903250222278_PopulateCategoryTable.cs
@@ -7,10 +7,14 @@ namespace BigSchool.Migrations
     {
         public override void Up()
         {
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (1,'Development')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (2,'Business')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (3,'Marketing')");
         }
         
         public override void Down()
         {
+
         }
     }
 }
