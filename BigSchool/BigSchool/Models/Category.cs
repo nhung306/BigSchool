def3679e26d2c6a96726b0ad2f155590cb2a03f1
sinhvien <sinhvien>
2019-03-25 09:24:40 +0700

    first commit

---
+++ BigSchool/BigSchool/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BigSchool.Models
+{
+    public class Category
+    {
+       public byte Id { get; set; }
+       [Required]
+       [StringLength(225)]
+       public string Name { get; set; }
+    }
+}
