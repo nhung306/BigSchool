350250cf9239e1350b30241e63b28dc6c4cdf884
Nhung <tnhung306>
2019-04-01 10:26:33 +0700

    zc

--- BigSchool/BigSchool/Models/ApplicationDbContext.cs
+++ BigSchool/BigSchool/Models/ApplicationDbContext.cs
@@ -15,6 +15,7 @@ namespace BigSchool.Models
     {
         public DbSet<Course> Courses { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -23,6 +24,14 @@ namespace BigSchool.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Course)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
